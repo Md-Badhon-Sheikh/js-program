ee9b6c725ab8e4c0f3653387504c7e18e80d1d63
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-11-02 10:00:23 +0600

    added more files

--- fibonacci.js
+++ fibonacci.js
@@ -12,14 +12,24 @@
 // }
 
 
-let n = 5;
-let a = 0;
-let b = 1;
+// let n = 5;
+// let a = 0;
+// let b = 1;
 
+// console.log(a);
+// for (let i = 1; i < 5; i++) {
+//     console.log(b);
+//     let next = a + b;
+//     a = b;
+//     b = next;
+// }
+
+let a = 0, b = 1, n = 5;
 console.log(a);
-for (let i = 1; i < 5; i++) {
+
+for(let i = 1; i<5; i++){
     console.log(b);
-    let next = a + b;
+    let next = a+b;
     a = b;
-    b = next;
+    b=next;
 }
